083d78d7433cf9a0766a667e8264d4944b34d1b9
Pål Moen Møst <pal.most@autodesk.com>
2022-11-05 15:17:40 -0400

    second

--- glue/main.ts
+++ glue/main.ts
@@ -1,4 +1,25 @@
-const main= () => {
+import {create, ShapeDiverSdkApiResponseType} from "@shapediver/sdk.geometry-api-sdk-v2";
 
-    console.log("hello")
+const main = async () => {
+
+    const sdk = create("https://sdr7euc1.eu-central-1.shapediver.com")
+    const res = await sdk.session.init()
+    //@ts-ignore
+
+    const toDownload = []
+    //@ts-ignore
+    Object.keys(res.outputs).forEach(x => {
+        //@ts-ignore
+        toDownload.push(res.outputs[x].content[0].href)
+    })
+    //@ts-ignore
+    console.log(toDownload)
+    //@ts-ignore
+    const file = awsdk.utils.download(toDownload[0], ShapeDiverSdkApiResponseType.DATA)
+    console.log(file)
 }
+
+main().then(res => {
+
+    console.log(res)
+})
