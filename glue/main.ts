772f6b7c32d4860847cca519cbfd2e026c057af4
Pål Moen Møst <pal.most@autodesk.com>
2022-11-05 16:08:45 -0400

    third thrid

--- glue/main.ts
+++ glue/main.ts
@@ -11,6 +11,8 @@ export const init = async () => {
     console.log("Hello from init");
     const sdk = create("https://sdr7euc1.eu-central-1.shapediver.com")
 };
+
+
 export const update = async (parameters: { [key: string]: any }) => {
     const sdk = create("https://sdr7euc1.eu-central-1.shapediver.com")
     const res = await sdk.session.init("")
@@ -24,8 +26,10 @@ export const commit = async () => {
 
 };
 
-const parameters = {"8937f2a7-33ad-4d9d-aa62-2adb10bbfdce": 4}
 
-update(parameters).then(() => {
-    console.log("success!")
-})
+
+//const parameters = {"8937f2a7-33ad-4d9d-aa62-2adb10bbfdce": 4}
+//
+// update(parameters).then(() => {
+//     console.log("success!")
+// })
