ba97d761b38c82d2165f8c564c4d09ef971c6034
Pål Moen Møst <pal.most@autodesk.com>
2022-11-05 15:47:35 -0400

    second third

--- glue/main.ts
+++ glue/main.ts
@@ -1,32 +1,31 @@
 import {create, ShapeDiverSdkApiResponseType} from "@shapediver/sdk.geometry-api-sdk-v2";
 
-const justGiveMeLinks =(outputs : {[key: string] : any}) => {
-    const toDownload :string[] = []
+const justGiveMeLinks = (outputs: { [key: string]: any }) => {
+    const toDownload: string[] = []
     Object.keys(outputs).forEach(x => {
         toDownload.push(outputs[x].content[0].href)
     })
     return toDownload
 }
-const main = async () => {
-
+export const init = async () => {
+    console.log("Hello from init");
+    const sdk = create("https://sdr7euc1.eu-central-1.shapediver.com")
+};
+export const update = async (parameters: { [key: string]: any }) => {
     const sdk = create("https://sdr7euc1.eu-central-1.shapediver.com")
     const res = await sdk.session.init("")
-    //@ts-ignore
-
-    //@ts-ignore
-    const currentLinks = justGiveMeLinks(res.outputs)
-
-   // const file = sdk.utils.download(toDownload[0], ShapeDiverSdkApiResponseType.DATA)
-
-    const newModel = await sdk.utils.submitAndWaitForCustomization(sdk, res.sessionId!, {"8937f2a7-33ad-4d9d-aa62-2adb10bbfdce": 4}, 30)
+    const newModel = await sdk.utils.submitAndWaitForCustomization(sdk, res.sessionId!, parameters, 30)
     //@ts-ignore
     const newLinks = justGiveMeLinks(newModel.outputs)
-    console.log(newLinks)
-    //  console.log(res)
+    const data = sdk.utils.download(newLinks[0], ShapeDiverSdkApiResponseType.DATA)
+    return newLinks
+};
+export const commit = async () => {
 
-}
+};
 
-main().then(res => {
+const parameters = {"8937f2a7-33ad-4d9d-aa62-2adb10bbfdce": 4}
 
-    console.log(res)
+update(parameters).then(() => {
+    console.log("success!")
 })
